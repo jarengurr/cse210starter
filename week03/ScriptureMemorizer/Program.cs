25c1a6c4ba32f66f2a2a40bfb3ca6dce8974fa1e
jarengurr <jarenwheat@gmail.com>
2025-09-27 22:48:37 -0700

    good update

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,7 +6,7 @@ class Program
 
     static void Main(string[] args)
     {
-        string theScripture = "3 Wherefore, I write unto my people, unto all those that shall receive hereafter these things which I write, that they may know the judgments of God, that they come upon all nations, according to the word which he hath spoken.";
+        string theScripture = " Wherefore, I write unto my people, unto all those that shall receive hereafter these things which I write, that they may know the judgments of God, that they come upon all nations, according to the word which he hath spoken.";
         Reference theRef = new Reference();
         theRef.SetBook("2nd Nephi");
         theRef.SetChapter(25);
@@ -14,27 +14,44 @@ class Program
         //theRef.SetEndVerse(3);
 
 
-
+        string answer=" ";
         string fullScripture;
         Scripture aScripture = new Scripture(theRef, theScripture);
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+         Console.Clear();
+        Console.Write($" {theRef.GetBook()} {theRef.GetChapter()}:{theRef.GetVerse()   }");
+       
         //aScripture.HideRandomWords(3);
         fullScripture = aScripture.GetDisplayText();
         Console.WriteLine(fullScripture);
-        string answer=" ";
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press Enter to continue or type quit to end.");
+        answer = Console.ReadLine();
+        
         while (answer != "quit")
         {
             Console.Clear();
             aScripture.HideRandomWords(3);
             fullScripture = aScripture.GetDisplayText();
+            Console.Write($" {theRef.GetBook()} {theRef.GetChapter()}:{theRef.GetVerse()}  ");
             Console.WriteLine(fullScripture);
             if (aScripture.IsCompletelyHidden())
             {
                 answer = "quit";
+                Console.WriteLine();
+                Console.WriteLine();
                 Console.WriteLine("Finished. Good Job");
             }
             else
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to continue or type quit to end.");
                 answer = Console.ReadLine();
+            }
+                
         }
     }
 }
